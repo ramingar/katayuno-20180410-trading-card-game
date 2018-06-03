9f76d55a52fb97f4f29966442e59705d582bc8cf
Rafael Minguet <rminguet@gmail.com>
2018-06-03 20:15:47 +0200

    Day 29-05-2018

--- public/assets/js/components/Player.js
+++ public/assets/js/components/Player.js
@@ -1,8 +1,9 @@
 import Deck from './Deck';
 import {pipe} from 'ramda';
 
-const Player = function ({life = 30, mana = 0, deck = Deck()} = {}) {
+const Player = function ({hand = [], life = 30, mana = 0, deck = Deck()} = {}) {
 
+    const getHand             = () => hand;
     const getLife             = () => life;
     const getManaSlot         = () => mana;
     const getDeck             = () => deck.getDeck();
@@ -14,18 +15,23 @@ const Player = function ({life = 30, mana = 0, deck = Deck()} = {}) {
 
     const getRandomFrom        = max => min => Math.floor(Math.random() * (max - min + 1) + min);
     const toLastIndexOfDeck    = pipe(getDeck, getDeckLength, getDeckLastIndex);
-    const getRandomIndexInDeck = () => getRandomFrom(0)(toLastIndexOfDeck());
+    const getRandomIndexInDeck = () => getRandomFrom(0)(toLastIndexOfDeck);
 
     const extractCard = (cards, cardIndex = getRandomIndexInDeck()) => {
-        const card    = getCardFromDeck(cards, cardIndex);
-        const newDeck = extractCardFromDeck(cards, cardIndex);
+        const card = getCardFromDeck(cards, cardIndex);
+        const deck = extractCardFromDeck(cards, cardIndex);
 
-        return {card, newDeck, cardIndex};
+        return {card, deck, cardIndex};
     };
 
-    const getState = () => Object.freeze({life: getLife(), mana: getManaSlot(), deck: getDeck()});
+    const addCardToHand = ({card, hand, ...rest}) => {
+        hand.push(card);
+        return {card, hand, ...rest};
+    };
 
-    return Object.freeze({getLife, getManaSlot, getDeck, getState, extractCard, setDeck});
+    const getState = () => Object.freeze({hand: getHand(), life: getLife(), mana: getManaSlot(), deck: getDeck()});
+
+    return Object.freeze({getLife, getManaSlot, getDeck, getState, extractCard, setDeck, addCardToHand});
 };
 
 export default Player;
--- tests/trading-card-game.test.js
+++ tests/trading-card-game.test.js
@@ -70,15 +70,64 @@ test(`-------- Component: Testing random withdraw`, (assert) => {
     const messageNumberCards   = `Deck must have one card less in its collection after a random withdraw.`;
     const messageCardExtracted = `Testing resulting card...`;
     const cards                = [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8];
-    const deck                 = Deck({cards});
+    const deckMock             = Deck({cards});
 
-    const player                     = Player({deck});
-    const {cardIndex, newDeck, card} = player.extractCard(player.getDeck());
-    const newCards                   = cards.filter((val, idx) => idx !== cardIndex);
-    player.setDeck(newDeck);
+    const player                  = Player({deckMock});
+    const {cardIndex, deck, card} = player.extractCard(player.getDeck());
+    const newCards                = cards.filter((val, idx) => idx !== cardIndex);
+    player.setDeck(deck);
 
     const playerDeck = player.getDeck();
     assert.deepEqual(playerDeck, newCards, messageNumberCards);
     assert.deepEqual(card, cards[cardIndex], messageCardExtracted);
     assert.end();
 });
+
+test(`-------- Component: Testing first hand`, (assert) => {
+    const messageNumberCards    = `Player must have three cards in his first hand.`;
+    const expectedNumberOfCards = 3;
+
+    const player = Player();
+
+    const {card1} = player.extractCard(player.getDeck());
+    let hand      = player.getState().hand;  // cambiar el hand para que no esté acoplado
+    player.addCardToHand({card1, hand});
+
+    const {card2} = player.extractCard(player.getDeck());
+    player.addCardToHand({card2, hand});
+
+    const {card3} = player.extractCard(player.getDeck());
+    player.addCardToHand({card3, hand});
+
+    const currentPlayerHand = player.getState().hand;
+    assert.equal(currentPlayerHand.length, expectedNumberOfCards, messageNumberCards);
+    assert.end();
+});
+
+/*
+test(`-------- Component: Testing game state`, (assert) => {
+    const messageTurn    = `Testing turn.`;
+    const messagePlayers = `Testing players.`;
+    const expectedTurn = 3;
+
+    const player = GameController();
+
+
+    const currentPlayerHand = player.getState().hand;
+    assert.equal(currentPlayerHand.length, expectedNumberOfCards, messageNumberCards);
+    assert.end();
+});
+*/
+
+/*
+test(`-------- Component: Testing player receives one mana slot each turn`, (assert) => {
+    const messageNumberCards    = `Testing player `;
+    const expectedNumberOfCards = 3;
+
+    const player = Player();
+
+    const currentPlayerHand = player.getState().hand;
+    assert.equal(currentPlayerHand.length, expectedNumberOfCards, messageNumberCards);
+    assert.end();
+});
+*/
